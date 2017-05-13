6d15fb8cf295dcd9dc749169b63907e982a6a043
JordLoopgit config --global user.email jordloop@gmail.com <jordloop@MacBook-Air.local>
2017-05-13 13:29:31 -0700

    Add function to collect values of checked boxed into an array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
-var toppingsArray = []
 
 $(document).ready(function() {
   $("form.user-form").submit(function(event) {
     event.preventDefault();
     var pizzaSize = $("#pizzaSize").val();
-  });
-});
+    var toppingsArray = []
+    $("input:checkbox[name=topping]:checked").each(function() {
+      var thisTopping = $(this).val();
+      toppingsArray.push(thisTopping);
+    }); // checkbox.each function
+  }); // submit button
+}); // Doc ready
