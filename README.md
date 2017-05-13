ac8bac218d1a82e79e2a3ca11455661b2ed98d55
JordLoopgit config --global user.email jordloop@gmail.com <jordloop@MacBook-Air.local>
2017-05-13 14:09:26 -0700

    Add specs to README.md, Pizza constructor, and Pizza constuctor initalization.

--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 
 ## Objective:
 Create a website for a pizza company where a user can choose one or more individual toppings and size to order a pizza and see the final cost.
-* Allow the user to choose toppings and size for the piza they'd like to order.
+* Allow the user to choose toppings and size for the pizza they'd like to order.
 * Create a pizza object constructor with properties for toppings and size.
 * Create a prototype method for the cost of a pizza depending on the selection chosen. User your own formula for this.
 
 ## Specs
+
+User selects pizza size and toppings and a price is returned.
+Input: User selects "personal" size and 3 toppings.  Output: $6.
--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,22 @@
+// Biz Logic
+var pizzaSize
+var toppingsArray
+function Pizza(size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+};
 
+// UI Logic
 $(document).ready(function() {
   $("form.user-form").submit(function(event) {
     event.preventDefault();
-    var pizzaSize = $("#pizzaSize").val();
-    var toppingsArray = []
+    debugger;
+    pizzaSize = $("#pizzaSize").val();
+    toppingsArray = []
     $("input:checkbox[name=topping]:checked").each(function() {
       var thisTopping = $(this).val();
       toppingsArray.push(thisTopping);
     }); // checkbox.each function
+    pizzaOrder = new Pizza(pizzaSize, toppingsArray);
   }); // submit button
 }); // Doc ready
