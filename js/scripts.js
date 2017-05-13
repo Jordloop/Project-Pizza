16ad6d072c2f9bfff4985416014ae6dd741c3925
JordLoopgit config --global user.email jordloop@gmail.com <jordloop@MacBook-Air.local>
2017-05-13 14:30:52 -0700

    Add Pizza.prototype.orderTotal to calculate the total cost of order.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,39 @@
-// Biz Logic
+// Biz Logic-------------------------
+
+ // DECLARE GLOBAL VARIABLES
 var pizzaSize
-var toppingsArray
+var toppingsArray = [];
+ //PIZZA CONSTRUCTOR
 function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
 };
+ // CALCULATES TOTAL PRICE OF A PIZZA
+Pizza.prototype.orderTotal = function() {
+  if (this.toppings.length <= 1) {
+    return this.size
+  } else if (this.toppings.length > 1) {
+    return this.size + (this.toppings.length - 1)
+  } else {
+    console.log("error in pizza.oderTotal");
+  }
+} //Pizza.orderTotal
 
-// UI Logic
+// UI Logic-------------------------
 $(document).ready(function() {
   $("form.user-form").submit(function(event) {
     event.preventDefault();
-    debugger;
-    pizzaSize = $("#pizzaSize").val();
-    toppingsArray = []
+
+    // COLLECTS INPUT
+    pizzaSize = parseInt($("#pizzaSize").val());
     $("input:checkbox[name=topping]:checked").each(function() {
       var thisTopping = $(this).val();
       toppingsArray.push(thisTopping);
     }); // checkbox.each function
+
+    // INITALIZES PIZZA CONSTRUCTOR
     pizzaOrder = new Pizza(pizzaSize, toppingsArray);
+    var test =  pizzaOrder.orderTotal();
+    console.log("total: " + test);
   }); // submit button
 }); // Doc ready
