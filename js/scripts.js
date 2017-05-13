1628017423f95cf85468b22a9539122a3d2e1e1d
JordLoopgit config --global user.email jordloop@gmail.com <jordloop@MacBook-Air.local>
2017-05-13 13:04:25 -0700

    Add HTML checkboxes to select toppings.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
+var toppingsArray = []
+
 $(document).ready(function() {
   $("form.user-form").submit(function(event) {
     event.preventDefault();
     var pizzaSize = $("#pizzaSize").val();
-    console.log(pizzaSize);
-
   });
 });
